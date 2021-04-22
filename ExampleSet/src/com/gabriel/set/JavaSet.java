569d912ffba0142a9f06cb5742f473c282a25f89
Gabriel Vogel <42189591+Vogel1212@users.noreply.github.com>
2021-04-22 14:23:29 -0300

    Alteração ExampleSet

---
+++ ExampleSet/src/com/gabriel/set/JavaSet.java
@@ -0,0 +1,20 @@
+package com.gabriel.set;
+import java.util.*;
+
+
+public class JavaSet {
+
+	 public static void main(String[] args)
+	    {
+	        // Exemplo de set usando HashSet
+	        Set<String> hash_Set = new HashSet<String>();
+	 
+	        hash_Set.add("A");
+	        hash_Set.add("B");
+	        hash_Set.add("C");
+	        hash_Set.add("D");
+	        hash_Set.add("Set");
+	 
+	        System.out.println(hash_Set);
+	    }
+	}
